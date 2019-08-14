438ced0f12ed63bfd6efc2285d2f3e0bcbdf8da8
Ana <atripunoska@gmail.com>
2019-08-14 23:15:58 +0200

    Fixed route to NYT Best Sellers

--- routes/best-sellers.js
+++ routes/best-sellers.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/best-sellers', function(req, res) {
+router.get('/', function(req, res) {
     
     res.render('best-sellers/index')
 });
